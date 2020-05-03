f73d331d846e004bc17459c07d8d30cb8f2e70fd
Evandro Leopoldino Gonçalves <evandrolgoncalves@gmail.com>
2020-05-03 20:27:01 +0200

    improve html structure of launch page

--- src/client/src/components/Launch.tsx
+++ src/client/src/components/Launch.tsx
@@ -1,14 +1,13 @@
 import React from "react";
 import gql from "graphql-tag";
 import { useQuery } from "@apollo/react-hooks";
-import { RouteComponentProps } from "react-router-dom";
+import { RouteComponentProps, Link } from "react-router-dom";
 
 const query = gql`
   query LaunchQuery($flight_number: Int!) {
     launch(flight_number: $flight_number) {
       flight_number
       mission_name
-      launch_year
       launch_success
       launch_date_local
       rocket {
@@ -20,16 +19,55 @@ const query = gql`
   }
 `;
 
-const Launch = ({ match }: RouteComponentProps<{ flight_number: string }>) => {
+type PropType = RouteComponentProps<{ flight_number: string }>;
+
+const Launch = ({ match }: PropType) => {
   const { loading, data } = useQuery(query, {
-    variables: { flight_number: match.params.flight_number },
+    variables: { flight_number: parseInt(match.params.flight_number, 10) },
   });
 
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+
+  const {
+    mission_name,
+    flight_number,
+    launch_success,
+    rocket: { rocket_id, rocket_type, rocket_name },
+  } = data.launch;
+
   return (
-    <>
-      <h1>Launch</h1>
-      {loading ? <p>Loading...</p> : console.log(data)}
-    </>
+    <div>
+      <h1>{mission_name}</h1>
+      <Link to="/">Back</Link>
+
+      <h3>Launch Details</h3>
+      <ul>
+        <li>
+          <strong>Launch Success:</strong> {launch_success}
+        </li>
+
+        <li>
+          <strong>Flight number:</strong> {flight_number}
+        </li>
+      </ul>
+
+      <h3>Rocket Details</h3>
+      <ul>
+        <li>
+          <strong>Rocket ID:</strong> {rocket_id}
+        </li>
+
+        <li>
+          <strong>Rocket Name:</strong> {rocket_name}
+        </li>
+
+        <li>
+          <strong>Rocket Type:</strong> {rocket_type}
+        </li>
+      </ul>
+    </div>
   );
 };
 
